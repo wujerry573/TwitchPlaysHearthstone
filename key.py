5a2d967142965a3df860558f62eb29267e6cd507
Jerry Wu <wujerry573@gmail.com>
2018-06-20 19:22:54 -0500

    first prototype

--- key.py
+++ key.py
@@ -2,239 +2,58 @@ import pyautogui
 import time
 import sys
 
-# def click(x,y):
-#     pyautogui.click((x, y))
-#
-# click(10, 10)
-
-
-def facemove():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def endmove():
-    pyautogui.moveTo(722, 503, 0.5, pyautogui.easeOutQuad)
-
-
-def heromove():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def hpmove():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h1move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h2move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h3move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h4move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h5move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h6move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h7move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h8move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h9move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def h10move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b1move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b2move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b3move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b4move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b5move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b6move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b7move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b8move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b9move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b10move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b11move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b12move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def b13move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e1move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e2move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e3move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e4move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e5move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e6move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e7move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e8move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e9move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e10move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e11move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e12move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-
-def e13move():
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-
-dic = {
-
-}
 
 keyMap = {
-  "FACE": facemove,
-  "HERO": heromove,
-  "HP": hpmove,
-  "END": endmove,
-  "H1": h1move,
-  "H2": h2move,
-  "H3": h3move,
-  "H4": h4move,
-  "H5": h5move,
-  "H6": h6move,
-  "H7": h7move,
-  "H8": h8move,
-  "H9": h9move,
-  "H10": h10move,
-  "B1": b1move,
-  "B2": b2move,
-  "B3": b3move,
-  "B4": b4move,
-  "B5": b5move,
-  "B6": b6move,
-  "B7": b7move,
-  "B8": b8move,
-  "B9": b9move,
-  "B10": b10move,
-  "B11": b11move,
-  "B12": b12move,
-  "B13": b13move,
-  "E1": e1move,
-  "E2": e2move,
-  "E3": e3move,
-  "E4": e4move,
-  "E5": e5move,
-  "E6": e6move,
-  "E7": e7move,
-  "E8": e8move,
-  "E9": e9move,
-  "E10": e10move,
-  "E11": e11move,
-  "E12": e12move,
-  "E13": e13move
+  "FACE": [962, 204],
+  "HERO": [959, 817],
+  "HP": [1133, 817],
+  "END": [1561, 489],
+  "H1": [623, 1057],
+  "H2": [693, 1057],
+  "H3": [756, 1057],
+  "H4": [807, 1057],
+  "H5": [862, 1057],
+  "H6": [918, 1057],
+  "H7": [971, 1057],
+  "H8": [1031, 1057],
+  "H9": [1090, 1057],
+  "H10": [1162, 1057],
+  "B1": [541, 583],
+  "B2": [611, 583],
+  "B3": [680, 583],
+  "B4": [746, 583],
+  "B5": [811, 583],
+  "B6": [885, 583],
+  "B7": [955, 583],
+  "B8": [1022, 583],
+  "B9": [1094, 583],
+  "B10": [1162, 583],
+  "B11": [1239, 583],
+  "B12": [1310, 583],
+  "B13": [1400, 583],
+  "E1": [545, 390],
+  "E2": [613, 390],
+  "E3": [682, 390],
+  "E4": [743, 390],
+  "E5": [818, 390],
+  "E6": [884, 390],
+  "E7": [952, 390],
+  "E8": [1017, 390],
+  "E9": [1087, 390],
+  "E10": [1159, 390],
+  "E11": [1232, 390],
+  "E12": [1300, 390],
+  "E13": [1381, 390]
 }
-
-
-def executecommand(key1, key2):
-    pyautogui.moveTo(834, 969, 0.5, pyautogui.easeOutQuad)
-    pyautogui.mouseDown(button='left')
-    pyautogui.moveTo(722, 503, 0.5, pyautogui.easeOutQuad)
-    pyautogui.mouseUp(button='left')
 
 
 time.sleep(1)
 key1 = sys.argv[1]
 key2 = sys.argv[2]
 
-keyMap[key1]()
+
+pyautogui.moveTo(keyMap[key1][0], keyMap[key1][1], 0.5, pyautogui.easeOutQuad)
 pyautogui.mouseDown(button='left')
-keyMap[key2]()
+pyautogui.moveTo(keyMap[key2][0], keyMap[key2][1], 0.5, pyautogui.easeOutQuad)
 pyautogui.mouseUp(button='left')
 
-# executecommand(key1, key2)
-#
-# if key1 in keyMap:
-#     print(keyMap[key1])
-
-
-# print('hello!')
