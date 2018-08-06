231c5d648be6a0be7228e0e09083fb24d0be595f
Jerry Wu <wujerry573@gmail.com>
2018-08-06 09:49:56 -0500

    d and a mode bug fix

--- chatbot.js
+++ chatbot.js
@@ -1,18 +1,19 @@
 const tmi = require("tmi.js");
 const config = require("./config");
 const exec = require("child_process").exec;
+const _unct = require("underscore");
 
 let client = new tmi.client(config.config);
 
 //default mode is anarchy, or false. true is democray
 let government = true;
+let voteCounter = 0;
 
 client.connect();
 client.on("connected", connectionCheck);
 client.on("whisper", rtv);
 
-client.on("message", modeChecker);
-
+client.on("message", readMessage);
 // if (government) {
 //   console.log(government);
 //   client.on("message", democracyMode);
@@ -29,15 +30,16 @@ function connectionCheck(address, port) {
 async function rtv(from, userstate, message, self) {
   if (self) return;
   let target = "#darwinnn_";
+  config.voteOptions.ANARCHY = 0;
+  config.voteOptions.DEMOCRACY = 0;
+
   if (userstate.username === "darwinnn_") {
     client.say(target, "Vote for anarchy or democracy!!!");
     client.say(target, "Vote for anarchy or democracy!!!");
   }
-  for (let key in config.votes) {
-    config.votes[key] = 0;
-  }
-  console.log(config.votes);
-  await new Promise(resolve => setTimeout(resolve, 30000));
+
+  console.log("config.voteoptions: ", config.voteOptions);
+  await new Promise(resolve => setTimeout(resolve, 10000));
   if (government && config.voteOptions.ANARCHY > config.voteOptions.DEMOCRACY) {
     client.say(target, "Anarchy reigns!!!");
     government = !government;
@@ -74,51 +76,61 @@ async function rtv(from, userstate, message, self) {
   config.voteOptions.DEMOCRACY = 0;
 }
 
-function anarchyMode(target, context, message, self) {
+async function readMessage(target, context, message, self) {
   if (self) return;
-  console.log("in anarchy");
-  let command = message.toUpperCase();
-  if (command === "ANARCHY") {
-    config.voteOptions.ANARCHY++;
-  }
-  if (command === "DEMOCRACY") {
-    config.voteOptions.DEMOCRACY++;
-  }
+  if (!government) {
+    let command = message.toUpperCase();
+    if (command === "ANARCHY") {
+      config.voteOptions.ANARCHY++;
+      console.log("voteOptions: ", config.voteOptions);
+    }
+    if (command === "DEMOCRACY") {
+      config.voteOptions.DEMOCRACY++;
+      console.log("voteOptions: ", config.voteOptions);
+    }
 
-  if (config.keyMap.includes(command)) {
-    if (config.config.os === "windows") {
-      exec("key.py " + command);
-    } else {
-      exec("python3 key.py " + command);
+    if (config.keyMap.includes(command)) {
+      if (config.config.os === "windows") {
+        exec("key.py " + command);
+      } else {
+        exec("python3 key.py " + command);
+      }
     }
   }
-}
+  if (government) {
+    if (self) return;
+    console.log("in democracy");
+    let command = message.toUpperCase();
 
-function democracyMode(target, context, message, self) {
-  if (self) return;
-  console.log("in democracy");
-  let command = message.toUpperCase();
-  let voteCounter = 0;
-  if (command === "ANARCHY") {
-    config.voteOptions.ANARCHY++;
-  }
-  if (command === "DEMOCRACY") {
-    config.voteOptions.DEMOCRACY++;
-  }
-  if (config.keyMap.includes(command)) {
-    config.votes[command]++;
-    voteCounter++;
-    if (voteCounter === 20) {
-      let winning = countVote();
-      if (config.config.os === "windows") {
+    if (command === "ANARCHY") {
+      config.voteOptions.ANARCHY++;
+      console.log("voteOptions: ", config.voteOptions);
+    }
+    if (command === "DEMOCRACY") {
+      config.voteOptions.DEMOCRACY++;
+      console.log("voteOptions: ", config.voteOptions);
+    }
+    if (config.keyMap.includes(command)) {
+      config.votes[command]++;
+      voteCounter++;
+      console.log("votes: ", voteCounter);
+      if (voteCounter === 5) {
+        let winning = countVote();
+        voteCounter = 0;
         console.log(winning);
-        exec("key.py " + winning[0]);
-        exec("key.py " + winning[1]);
-      } else {
-        exec("python3 key.py " + winning[0]);
-        exec("python3 key.py " + winning[1]);
+        if (config.config.os === "windows") {
+          await exec("key.py " + winning[0]);
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          await exec("key.py " + winning[1]);
+        } else {
+          await exec("python3 key.py " + winning[0]);
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          await exec("python3 key.py " + winning[1]);
+        }
+        for (let key in config.votes) {
+          config.votes[key] = 0;
+        }
       }
-      voteCounter = 0;
     }
   }
 }
@@ -128,30 +140,23 @@ function countVote() {
   let highest2 = 0;
   let current = 0;
   for (let key in config.votes) {
-    config.votes[key] = current;
+    current = config.votes[key];
     if (current > highest1) {
       highest1 = current;
     }
   }
+  current = 0;
   for (let key in config.votes) {
-    config.votes[key] = current;
-    if (current > highest2) {
+    current = config.votes[key];
+    if (current === highest1) {
+      current = 0;
+    } else if (current > highest2 && highest2 < highest1) {
       highest2 = current;
     }
-    if (highest2 > highest1) {
-      let temp = highest1;
-      highest1 = highest2;
-      highest2 = highest1;
-    }
   }
-  return [highest1, highest2];
-}
 
-function modeChecker() {
-  console.log("in modechecker ", government);
-  if (government) {
-    client.on("message", democracyMode);
-  } else {
-    client.on("message", anarchyMode);
-  }
+  return [
+    _unct.invert(config.votes)[highest1],
+    _unct.invert(config.votes)[highest2]
+  ];
 }
